246b31868d1528dee0b9c5ccf3d49b34fda0316e
Maksymilian Walczak <maksymilianwalczak95@gmail.com>
2023-03-20 21:07:53 +0100

    Zadanie domowe dzień 6

---
+++ Zadanie domowe dzień_ 6/Zadanie domowe dzień_ 6/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace Zadanie_domowe_dzień__6
+{
+    public class Employee
+    {
+        public Employee(string name, string SurName, int age, int points)
+        {
+            this.imię = name;
+            this.nazwisko = SurName;
+            this.wiek = age;
+            this.punkty = points;
+        }
+        public string imię { private set; get; }
+        public string nazwisko { private set; get; }
+        public int wiek { private set; get; }
+        public int punkty { private set; get; }
+
+        List<int> Points = new List<int>();
+        public int Result
+        {
+            get
+            { return this.Points.Sum(); }
+        }
+        public void AddPoints(int points)
+        {
+            Points.Add(points);
+        }
+    }
+}
---
+++ Zadanie domowe dzień_ 6/Zadanie domowe dzień_ 6/Program.cs
@@ -0,0 +1,66 @@
+﻿using Zadanie_domowe_dzień__6;
+
+Employee employee1 = new Employee("Marzenka", "Gandi", 22, 0);
+Employee employee2 = new Employee("Staszek", "Fistaszek", 41, 0);
+Employee employee3 = new Employee("Fausti", "Gutierez", 19, 0);
+
+employee1.AddPoints(14);
+employee1.AddPoints(3);
+
+employee2.AddPoints(9);
+employee2.AddPoints(8);
+
+employee3.AddPoints(3);
+employee3.AddPoints(5);
+
+List<Employee> list = new List<Employee>()
+    {
+        employee1, employee2, employee3
+    };
+
+int HighestPoints = 0;
+List<string> HighestImię = new List<string>();
+List<string> HighestNazwisko = new List<string>();
+List<int> HighestWiek = new List<int>();
+
+foreach (Employee employee in list)
+{
+    if (employee.Result >= HighestPoints)
+    {
+        HighestPoints = employee.Result;
+    }
+}
+foreach (Employee employee in list)
+{
+    if (employee.Result >= HighestPoints)
+    {
+        HighestPoints = employee.Result;
+        HighestImię.Add(employee.imię);
+        HighestNazwisko.Add(employee.nazwisko);
+        HighestWiek.Add(employee.wiek);
+    }
+}
+Console.Write("najwyższa uzyskana ilość punktów: ");
+Console.WriteLine(HighestPoints);
+int i = 1;
+foreach (string H_imie in HighestImię)
+{
+    Console.Write(i++);
+    Console.Write(". ");
+    Console.WriteLine(H_imie);
+  
+}
+i = 1;
+foreach (string H_nazwisko in HighestNazwisko)
+{
+    Console.Write(i++);
+    Console.Write(". ");
+    Console.WriteLine(H_nazwisko);
+}
+i = 1;
+foreach (int H_wiek in HighestWiek)
+{
+    Console.Write(i++);
+    Console.Write(". ");
+    Console.WriteLine(H_wiek);
+}
